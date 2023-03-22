a6f83e7e22a7e3ca308e15c60108170e0663195a
Coding_Buddha <priyanshraj3020@gmail.com>
2023-03-22 17:21:55 +0530

    [200~Minimum number of jumps~

--- Array/Daily_Practice.py
+++ Array/Daily_Practice.py
@@ -1,14 +1,11 @@
-def Find_min_and_max(arr,n):
-    Max = arr[0]
-    Min = arr[0]
-    for i in range(n):
-        if Max < arr[i]:
-            Max = arr[i]
-        elif Min > arr[i]:
-            Min = arr[i]
-    print("Max Element ==> ",Max)
-    print("Min Element ==> ",Min)
+def minJumps(arr):
+    pointer  = 0
+    count = 0
+    for i in range(len(arr)):
+        if i == pointer:
+            pointer = arr[i+1]
+            count +=1
+    return count
 
-array = [1,5,0,2,3,6,4,2,1]
-n = len(array)
-Find_min_and_max(array,n)
+arr = [1,3,5,8,9,2,6,7,6,8,9]
+print(minJumps(arr))
